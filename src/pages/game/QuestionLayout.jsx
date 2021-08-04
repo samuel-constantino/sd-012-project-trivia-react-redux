3316c0fe4c29220921b33aeceebc8318052097f0
Lucas Henrique da Silva Brito <69560297+hnq-brt@users.noreply.github.com>
2021-08-04 15:56:35 -0300

    [Grupo 6] Requisito 7 (#177)
    * [Grupo 6] Requisito 7
    
    * Correção ESLint

--- src/pages/game/QuestionLayout.jsx
+++ src/pages/game/QuestionLayout.jsx
@@ -2,15 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class QuestionLayout extends React.Component {
+  constructor() {
+    super();
+    this.checkAnswer = this.checkAnswer.bind(this);
+    this.state = {
+      correct: '',
+      wrong: '',
+    };
+  }
+
+  checkAnswer() {
+    this.setState({
+      correct: 'correct',
+      wrong: 'wrong',
+    });
+  }
+
   render() {
+    const { correct, wrong } = this.state;
     const { questions } = this.props;
     return (
       <>
         <h1 data-testid="question-category">{questions[0].category}</h1>
         <p data-testid="question-text">{questions[1].question}</p>
         <button
+          className={ correct }
           type="button"
           data-testid="correct-answer"
+          onClick={ this.checkAnswer }
         >
           {questions[0].correct_answer}
 
@@ -18,7 +37,13 @@ class QuestionLayout extends React.Component {
         {
           questions[0].incorrect_answers
             .map((e, index) => (
-              <button data-testid={ `wrong-answer-${index}` } type="button" key={ e }>
+              <button
+                className={ wrong }
+                data-testid={ `wrong-answer-${index}` }
+                type="button"
+                key={ e }
+                onClick={ this.checkAnswer }
+              >
                 {e}
               </button>))
         }
