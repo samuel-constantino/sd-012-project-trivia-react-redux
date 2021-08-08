a8cdf9c7f1ab049da3d8ba84a9cffb0c57d7ff74
MiguelRetroz <miguel.retroz@outlook.com>
2021-08-08 06:43:17 -0300

    Renderizando ranking

--- src/pages/ranking/index.js
+++ src/pages/ranking/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 class Ranking extends React.Component {
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        {
+          ranking.map((player, index) => (
+            <div key={ index }>
+              <img src={ player.picture } alt={ player.name } />
+              <span data-testid={ `player-name-${index}` }>{ player.name }</span>
+              <span data-testid={ `palyer-score${index}` }>{ player.score }</span>
+            </div>
+          ))
+        }
+      </>
     );
   }
 }
