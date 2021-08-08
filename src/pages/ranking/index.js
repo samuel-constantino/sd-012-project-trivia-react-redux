1c1db78f00bdce3ca43aa08f33cf0dcf43e0f738
MiguelRetroz <miguel.retroz@outlook.com>
2021-08-08 06:44:11 -0300

    Adiciona botão para `Jogar novamente`

--- src/pages/ranking/index.js
+++ src/pages/ranking/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
@@ -15,6 +16,11 @@ class Ranking extends React.Component {
             </div>
           ))
         }
+        <Link to="/">
+          <button data-testid="btn-go-home" type="button">
+            Jogar novamente
+          </button>
+        </Link>
       </>
     );
   }
